8f07871bf2d388a3fd853ec7426700e997cbd523
asurmen11 <asurmen_11@hotmail.com>
2022-04-14 21:12:12 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { html } from 'orison';
 export default context => html`
   <section>${context.mdFile('./src/partials/getting-started.md')}</section>
   <section>
-    <h3>Here are some example pages</h3>
+    <h3>TEST</h3>
     <ul>
       <li><a href="/blog.html">/blog.html</a></li>
       <li><a href="/blog/blog-a.html">/blog/blog-a.html</a></li>
